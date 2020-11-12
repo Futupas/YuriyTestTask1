58699848a846d9663f6271964c23875b2890d096
Olexandr Panov <futupas@gmail.com>
2020-11-12 11:57:40 +0200

    email input mask works

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 window.onload = (e) => {
     setTodayDateToInput();
-    // applyEmailMask();    
+    applyEmailMask();    
 
 
     document.getElementById('addTaskForm').onsubmit = async (e) => {
@@ -37,56 +37,14 @@ function setTodayDateToInput() {
     let instance = new dtsel.DTS('#addTaskForm input[name=endingDate]',  {
         showTime: false,
         showDate: true,
-        dateFormat: "dd.mm.yyyy",
+        dateFormat: 'dd.mm.yyyy',
         direction: 'BOTTOM'
     });
 }
 
 function applyEmailMask() {
-    /**
-     * @type HTMLElement
-     */
-    let emailInput = document.querySelector('#addTaskForm input[name=email]');
-    emailInput.onclick = (e) => {
-        if (emailInput.maskPosition === undefined && emailInput.realValue === undefined) {
-            emailInput.value = '@_._';
-            emailInput.selectionStart = 0;
-            emailInput.selectionEnd = 0;
-        }
-        
-        // return false;
-    }
-    emailInput.onkeydown = (e) => {
-        // ___0___@___1___.___2___
-        if (emailInput.maskPosition === undefined) emailInput.maskPosition = 0;
-        if (emailInput.realValue === undefined) emailInput.realValue = '';
-        let key = e.key;
-        if (key === 'Backspace') {
-            console.log(emailInput.selectionStart);
-            console.log(emailInput.selectionEnd);
-        } else if (key === 'Delete') {
-
-        }
-        if (emailInput.maskPosition === 0) {
-            emailInput.realValue += key;
-            if (key === '@') {
-                emailInput.value = emailInput.realValue + '_._';
-                emailInput.selectionStart = emailInput.realValue.length;
-                emailInput.selectionEnd = emailInput.realValue.length;
-                emailInput.maskPosition = 1;
-            } else {
-                emailInput.value = emailInput.realValue + '@_._';
-                emailInput.selectionStart = emailInput.realValue.length;
-                emailInput.selectionEnd = emailInput.realValue.length;
-            }
-        } else if (emailInput.maskPosition === 1) {
-            //
-        } else if (emailInput.maskPosition === 2) {
-            //
-        } else {
-
-        }
-        
-        return false;
-    }
+    Inputmask({'mask': '*{3,20}@*{3,20}.*{2,7}', }).mask(document.querySelector('#addTaskForm input[name=email]'));
 }
+
+
+
